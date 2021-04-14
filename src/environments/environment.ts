399a8a9aedcc7590ecee6ff168d49bf4a925fabf
Purvi <purvirawal.work@gmail.com>
2021-04-14 16:04:58 -0700

    chore: Change production environment variables

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,12 +3,12 @@
  */
 
 export const environment = {
-  production: false,
-  apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
+  production: true,
+  apiServerUrl: 'https://capstone-agency-backend.herokuapp.com', // the running FLASK api server url
   auth0: {
     url: 'purvi-udacity.us', // the auth0 domain prefix
     audience: 'capstone_api', // the audience set for the auth0 app
     clientId: '6TY6xaOUYvkQNrgcS3WR0clXpW99IBAM', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
+    callbackURL: 'https://capstone-movie-agency.herokuapp.com', // the base url of the running ionic application. 
   }
 };
